e7c42b435c467864b6ce9c48b36131113d77467c
ghdss-dev <ghdss_ti@hotmail.com>
2025-01-30 21:11:10 -0300

    Separando o feed e feed editor

--- core/Model.php
+++ core/Model.php
@@ -10,20 +10,28 @@ class Model {
     protected static $_h;
     
     public function __construct() {
+
         self::_checkH();
+        
     }
 
     public static function _checkH() {
+
         if(self::$_h == null) {
+
             $connection = Database::getInstance();
+
             self::$_h = new Builder('mysql', function($query, $queryString, $queryParameters) use($connection) {
+
                 $statement = $connection->prepare($queryString);
                 $statement->execute($queryParameters);
 
                 if ($query instanceof FetchableInterface)
+
                 {
                     return $statement->fetchAll(\PDO::FETCH_ASSOC);
                 }
+
             });
         }
         
@@ -31,29 +39,38 @@ class Model {
     }
 
     public static function getTableName() {
+
         $className = explode('\\', get_called_class());
         $className = end($className);
         return strtolower($className).'s';
+
     }
 
     public static function select($fields = []) {
+
         self::_checkH();
         return self::$_h->select($fields);
     }
 
     public static function insert($fields = []) {
+
         self::_checkH();
         return self::$_h->insert($fields);
+
     }
 
     public static function update($fields = []) {
+
         self::_checkH();
         return self::$_h->update($fields);
+
     }
 
     public static function delete() {
+
         self::_checkH();
         return self::$_h->delete();
+
     }
 
 }
