3a4511fe5141b281916272b7bddb11a71dc293b3
ghdss-dev <ghdss_ti@hotmail.com>
2025-02-04 20:22:39 -0300

    Telas de Conteúdo Especifico realizado

--- src/routes.php
+++ src/routes.php
@@ -4,11 +4,19 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/login', 'LoginController@signin');
 $router->post('/login', 'LoginController@signinAction');
+
 $router->get('/cadastro', 'LoginController@signup');
 $router->post('/cadastro', 'LoginController@signupAction');
+
 $router->post('/post/new', 'PostController@new');
+
+$router->get('/perfil/{id}', 'ProfileController@index');
+$router->get('/perfil', 'ProfileController@index');
+
+
 
 //$router->get('/pesquisar');
 //$router->get('/perfil');
